9f67913f154d4e150e543ac5dfd9285e15b90842
SubhamayDebnath <subhamaydebnath6@gmail.com>
2025-04-07 02:45:30 +0530

    :)

---
+++ LexicalAndClosure.js
@@ -0,0 +1,20 @@
+// function outer(){
+//     let username = "subhamay";
+//     function inner(){
+//         console.log(username);
+//     }
+//     inner();
+// }
+// outer();
+// function outer(){
+//     return function inner(){
+//         console.log("This is inner function text");
+//     }
+// }
+
+// function main(){
+//     const outer1 = outer();
+//     outer1();
+// }
+// main();
+
